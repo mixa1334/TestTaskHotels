5bb006423359bf2e94ad9f8dff93cb281723cfd1
mishaNYA <misheyzo@icloud.com>
2025-07-08 16:29:21 +0300

    fix: config

---
+++ src/main/java/com/hotels/hotels/Config.java
@@ -0,0 +1,20 @@
+package com.hotels.hotels;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.hotels.hotels.model.service.Histogram;
+
+@Configuration
+@EnableTransactionManagement
+public class Config implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(
+                String.class, Histogram.Type.class,
+                str -> Histogram.Type.fromString(str).orElseThrow(IllegalArgumentException::new));
+    }
+}
--- src/main/java/com/hotels/hotels/HotelsApplication.java
+++ src/main/java/com/hotels/hotels/HotelsApplication.java
@@ -2,10 +2,8 @@ package com.hotels.hotels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class HotelsApplication {
 
 	public static void main(String[] args) {
